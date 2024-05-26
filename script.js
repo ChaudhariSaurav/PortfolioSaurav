95df4bcd2b90cbfa16593f72dcaddbb101df0e34
Saurav kumar chaudhary <98453545+ChaudhariSaurav@users.noreply.github.com>
2024-05-26 20:33:22 +0000

    updates

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ document.getElementById('mobile-menu').addEventListener('click', function() {
     var nav = document.querySelector('#navbar ul');
     nav.classList.toggle('active');
 });
+
+function collapseNavbar() {
+    // Collapse the navbar if it's expanded
+    var navbarCollapse = document.querySelector('.navbar-collapse');
+    if (navbarCollapse.classList.contains('show')) {
+        navbarCollapse.classList.remove('show');
+    }
+}
